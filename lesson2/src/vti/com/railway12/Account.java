88b946d83e81c4b0abb51c1f1e4f3b2b7fd75127
creatone1996 <hieupt0807@gmail.com>
2021-06-08 17:33:55 +0700

    lesson2

---
+++ lesson2/src/vti/com/railway12/Account.java
@@ -0,0 +1,18 @@
+package vti.com.railway12;
+
+import java.util.Date;
+
+public class Account {
+	
+	int id;
+	String email;
+	String userName;
+	String fullName;
+	Department department;
+	Position position;
+	Date createDate;
+	Group[] groups;
+	}
+
+
+
